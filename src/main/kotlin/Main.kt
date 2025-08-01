c3b7380d1d4065d6051e934098ac8359af7c3359
Jonathan <johjo@ytreza.org>
2025-08-01 14:27:30 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/kotlin/Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,3 +5,15 @@ import java.sql.DriverManager.println
 fun main() {
     println("Hello World!")
 }
+
+
+
+
+/*
+
+ 4 x 4
+
+[0, 0, 2, 2] -> [0, 0, 0, 4]
+[0, 0, 0, 4] -> [0, 0, 0, 4]
+[4, 0, 0, 0] -> [0, 0, 0, 4]
+ */
