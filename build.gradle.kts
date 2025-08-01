bfd32a8ae1aff943788c8cd56e5aa32e56091b91
Jonathan Laurent <Johjo@users.noreply.github.com>
2025-08-01 13:53:26 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+version = "1.0-SNAPSHOT"
+group = "nl.codecraftr"
+
+plugins {
+    kotlin("jvm") version "1.9.23"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
+}
+
+application {
+    mainClass.set("kata.base.AppKt")
+}
